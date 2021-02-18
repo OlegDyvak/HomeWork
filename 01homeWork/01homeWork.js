c33a036c6ef6dae2dd02a8ff11807baa66410e37
OlegDyvak <dyvak.oleg@gmail.com>
2021-02-18 14:14:29 +0200

    -a

--- 01homeWork/01homeWork.js
+++ 01homeWork/01homeWork.js
@@ -1,15 +1,15 @@
 const potatoPrice = 15.678
 const onionPrice = 123.965
 const carrotPrice = 90.2345
-console.log(Math.max(potatoPrice, onionPrice, carrotPrice))
-console.log(Math.min(potatoPrice, onionPrice, carrotPrice))
+console.log("Максимальне значення:",Math.max(potatoPrice, onionPrice, carrotPrice))
+console.log("Мінімальне значення:",Math.min(potatoPrice, onionPrice, carrotPrice))
 const sumPrice = (potatoPrice + onionPrice + carrotPrice)
-console.log(sumPrice)
+console.log("Сумма:",sumPrice)
 const sumPriceFloor=(Math.floor(potatoPrice)+Math.floor(onionPrice)+Math.floor(carrotPrice))
 const sumPriceFloorRound = (Math.round(sumPriceFloor/100)*100)
-console.log(sumPriceFloorRound)
+console.log("Сумма округлена до сотень:",sumPriceFloorRound)
 let evenOrOdd = false
 if (sumPriceFloor % 2 == 0) evenOrOdd = true
-console.log("Перевірка на парність (парне-true, непарне-false):",evenOrOdd)
+console.log("Перевірка сумми на парність (парне-true, непарне-false):",evenOrOdd)
 console.log("Решта з 500:", (500 - sumPrice))
-console.log("Середнє значення откуглене до ругого знаку після коми:", (Math.round(100*(sumPrice/3))/100))
+console.log("Середнє значення округлене до другого знаку після коми:", (Math.round(100*(sumPrice/3))/100))
