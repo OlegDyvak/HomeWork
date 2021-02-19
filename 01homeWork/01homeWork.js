bf194abfdcdb2661e09a5d1c460854f9afd140a8
OlegDyvak <79101218+OlegDyvak@users.noreply.github.com>
2021-02-19 17:04:59 +0200

    Update 01homeWork.js

--- 01homeWork/01homeWork.js
+++ 01homeWork/01homeWork.js
@@ -13,3 +13,12 @@ if (sumPriceFloor % 2 == 0) evenOrOdd = true
 console.log("Перевірка сумми на парність (парне-true, непарне-false):",evenOrOdd)
 console.log("Решта з 500:", (500 - sumPrice))
 console.log("Середнє значення округлене до другого знаку після коми:", (Math.round(100*(sumPrice/3))/100))
+let discount = Math.random()
+console.log ("Сумма з випадковою знижкою:",(Math.round(100*(sumPrice*(1-discount))/100))
+console.log ("Чистий прибуток:",(sumPrice/2 - sumPrice*discount))
+
+//Advanced
+
+document.writeln(`Максимальне значення:${Math.max(potatoPrice, onionPrice, carrotPrice)}<br/>bla`)
+          
+             
