56712e2cec07a45637ee2c65abdac4afde7e69b2
OlegDyvak <dyvak.oleg@gmail.com>
2021-02-21 11:52:21 +0200

    final

--- 01homeWork/01homeWork.js
+++ 01homeWork/01homeWork.js
@@ -1,34 +1,49 @@
 const potatoPrice = 15.678
 const onionPrice = 123.965
 const carrotPrice = 90.2345
-console.log("Максимальне значення:",Math.max(potatoPrice, onionPrice, carrotPrice))
-console.log("Мінімальне значення:",Math.min(potatoPrice, onionPrice, carrotPrice))
+
+const maxPrice = Math.max(potatoPrice, onionPrice, carrotPrice)
+console.log("Максимальне значення:", maxPrice)
+
+const minPrice = Math.min(potatoPrice, onionPrice, carrotPrice)
+console.log("Мінімальне значення:", minPrice)
+
 const sumPrice = (potatoPrice + onionPrice + carrotPrice)
-console.log("Сумма:",sumPrice)
+console.log("Сумма:", sumPrice)
+
 const sumPriceFloor=(Math.floor(potatoPrice)+Math.floor(onionPrice)+Math.floor(carrotPrice))
 const sumPriceFloorRound = (Math.round(sumPriceFloor/100)*100)
-console.log("Сумма округлена до сотень:",sumPriceFloorRound)
+console.log("Сумма округлена до сотень:", sumPriceFloorRound)
+
 let evenOrOdd = false
-if (sumPriceFloor % 2 == 0) evenOrOdd = true
-console.log("Перевірка сумми на парність (парне-true, непарне-false):",evenOrOdd)
+if (sumPriceFloor % 2 === 0) evenOrOdd = true
+console.log("Перевірка сумми на парність (парне-true, непарне-false):", evenOrOdd)
+
 const bill = 500
-console.log("Решта з ",bill,":", (bill - sumPrice))
-console.log("Середнє значення округлене до другого знаку після коми:", (Math.round(100*(sumPrice/3))/100))
-let discount = Math.random()
-console.log("Сумма з випадковою знижкою:",(Math.round(100*(sumPrice*(1-discount))/100)))
-console.log("Чистий прибуток:",(sumPrice/2 - sumPrice*discount).toFixed(2))
+let change = bill - sumPrice
+console.log("Решта з ", bill, ":", change)
+
+const average = (sumPrice).toFixed(2)
+console.log("Середнє значення округлене до другого знаку після коми:", average)
+
+const discount = Math.random()
+const discountPrice = sumPrice * (1 - discount)
+console.log("Сумма з випадковою знижкою:", discountPrice.toFixed(2))
+
+const profit = sumPrice/2 - sumPrice*discount
+console.log("Чистий прибуток:", profit.toFixed(2))
 
 //Advanced
 
-document.writeln(`Максимальне значення: ${Math.max(potatoPrice, onionPrice, carrotPrice)} \
-Мінімальне значення: ${Math.min(potatoPrice, onionPrice, carrotPrice)} \
-Сумма: ${sumPrice} \
-Сумма округлена до сотень: ${sumPriceFloorRound} \
-Перевірка сумми на парність (парне-true, непарне-false): ${evenOrOdd} \
-Решта з ${bill}: ${bill - sumPrice} \
-Середнє значення округлене до другого знаку після коми: ${Math.round(100*(sumPrice/3))/100} \
-Сумма з випадковою знижкою: ${Math.round(100*(sumPrice*(1-discount))/100)} \
-Чистий прибуток: ${(sumPrice/2 - sumPrice*discount).toFixed(2)}                       
+document.writeln(`Максимальне значення: ${maxPrice} <br>
+Мінімальне значення: ${minPrice} <br>
+Сумма: ${sumPrice} <br>
+Сумма округлена до сотень: ${sumPriceFloorRound} <br>
+Перевірка сумми на парність (парне-true, непарне-false): ${evenOrOdd} <br>
+Решта з ${bill}: ${change} <br>
+Середнє значення округлене до другого знаку після коми: ${average} <br>
+Сумма з випадковою знижкою: ${discountPrice.toFixed(2)} <br>
+Чистий прибуток: ${profit.toFixed(2)}                       
 `)
           
              
