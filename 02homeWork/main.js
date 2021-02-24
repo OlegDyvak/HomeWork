ccfe155545696cdfe69576dbe74ed5400a511ef5
OlegDyvak <79101218+OlegDyvak@users.noreply.github.com>
2021-02-24 11:47:38 +0200

    Update main.js

--- 02homeWork/main.js
+++ 02homeWork/main.js
@@ -1,18 +1,31 @@
 const N = + prompt('Введіть перше число (N):')
 const M = + prompt('Введіть друге число (M):')
-if (!N || !M) alert('Ви ввели НЕ число!')
-if (!(Number.isInteger(N) || Number.isInteger(M))) alert('Ви ввели НЕ ціле число!')
+switch(true)
+    case (!N || !M){
+            alert('Ви ввели НЕ число!')
+            break
+            }
+    case (!(Number.isInteger(N) || Number.isInteger(M))){
+            alert('Ви ввели НЕ ціле число!')
+            break
+            }
+    case (N>=M){
+            alert('Перше число повинно бути менше другого!')
+            break
+            }
+//if (!N || !M) alert('Ви ввели НЕ число!')
+//if (!(Number.isInteger(N) || Number.isInteger(M))) alert('Ви ввели НЕ ціле число!')
 const isTrue = confirm('Виберіть "Ok" якщо потрібно пропускати парні числа, і "Отмена" якщо ні')
 let sum = 0
 let i = 0
 if (isTrue === true) {
     if (N % 2 === 0) i = N + 1
     else i = N
-    while (i <= M) {
-        sum = sum + i
-        i = i + 2
-        }  
-    }
+while (i <= M) {
+    sum = sum + i
+    i = i + 2
+    }  
+}
 else {
     i = N
     while (i <= M) {
@@ -20,4 +33,4 @@ else {
         i++
     }
 }
-console.log (`Сумма чисел від ${N} до ${M}: ${sum}`)
+alert (`Сумма чисел від ${N} до ${M}: ${sum}`)
