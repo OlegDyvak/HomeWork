5001658fd4a048cfb36c8c49e8ecdde8a54fea76
OlegDyvak <dyvak.oleg@gmail.com>
2021-03-10 20:04:34 +0200

    refactoring hw 2

--- 02homeWork/main.js
+++ 02homeWork/main.js
@@ -1,38 +1,32 @@
-const N = + prompt('Введіть перше число (N):')
-const M = + prompt('Введіть друге число (M):')
+let N = null
+do { N = + prompt('Введіть перше число (N):') }
+while (!Number.isInteger(N) || !N)
+
+let M = null
+do { M = + prompt('Введіть друге число (M):') }
+while (!Number.isInteger(M) || !M)
+
 switch(true){
     case (!N || !M):{
             alert('Ви ввели НЕ число!')
             break
-            }
+    }
     case (!Number.isInteger(N) || !Number.isInteger(M)):{
             alert('Ви ввели НЕ ціле число!')
             break
-            }
+    }
     case (N>=M):{
             alert('Перше число повинно бути менше другого!')
             break
-            }
-    case (N<M):{
-const isTrue = confirm('Виберіть "Ok" якщо потрібно пропускати парні числа, і "Отмена" якщо ні')
-let sum = 0
-let i = 0
-if (isTrue === true) {
-    if (N % 2 === 0) i = N + 1
-    else i = N
-while (i <= M) {
-    sum = sum + i
-    i = i + 2
-    }  
-}
-else {
-    i = N
-    while (i <= M) {
-        sum = sum + i
-        i++
     }
-}
-alert (`Сумма чисел від ${N} до ${M}: ${sum}`)
-break
-     }
+    case (N<M):{
+            const isTrue = confirm('Виберіть "Ok" якщо потрібно пропускати парні числа, і "Отмена" якщо ні')
+            let sum = 0
+            for (let index = N; index <= M; index ++) {
+                if (isTrue && (index % 2 === 0)) continue
+                sum += index
+            } 
+            alert (`Сумма чисел від ${N} до ${M}: ${sum}`)
+            break
+    }
 }
