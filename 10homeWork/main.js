2b0b32fe1fae646cdc978a8dd587c4a7a7dbac4a
OlegDyvak <dyvak.oleg@gmail.com>
2021-04-23 12:53:06 +0300

    hw 10 done

--- 10homeWork/main.js
+++ 10homeWork/main.js
@@ -1,39 +1,17 @@
 function generateBlocks() {
-    document.body.innerHTML =
-    `<div style = "width:250px; height:50px"><div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div></div>
-    
-    <div style = "width:250px; height:50px"><div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div></div>
-    
-    <div style = "width:250px; height:50px"><div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div></div>
-    
-    <div style = "width:250px; height:50px"><div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div></div>
-    
-    <div style = "width:250px; height:50px"><div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div>
-    <div style = "background-color:${getRandomColor()}; width:50px; height:50px; float: left"></div></div>`
-
+  document.body.innerHTML = ''
+  for (let i = 0; i < 5; i++) {
+    document.body.innerHTML += `<div class="blocks"></div>`
+  }
+  const blocksList = document.body.querySelectorAll(".blocks")
+  for (let i = 0; i < 5; i++) {
+    blocksList.forEach((element) => { element.innerHTML += `<div class="oneblock" style = "background-color:${getRandomColor()}"></div>` });
+  }
 }
 
 function generateBlocksInterval() {
-    setInterval (generateBlocks, 1000)
+  document.body.innerHTML = ''
+  setInterval (generateBlocks, 1000)
 }
 
 function getRandomColor() {
